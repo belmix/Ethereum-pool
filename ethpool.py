65a4fb3c50881863ad4b2842d0e84ee0fc133ade
belmix <djdram@ya.ru>
2016-08-29 00:10:52 +0300

    Update ethpool.py

--- ethpool.py
+++ ethpool.py
@@ -12,22 +12,8 @@ from threading import Thread, Lock
 import Queue
 from ethjsonrpc import EthJsonRpc
 methods = [
-    'web3_clientVersion',
-    'net_version',
-    'net_peerCount',
-    'net_listening',
-    'eth_protocolVersion',
-    'eth_syncing',
-    'eth_coinbase',
-    'eth_mining',
     'eth_hashrate',
-    'eth_gasPrice',
     'eth_accounts',
-    'eth_blockNumber',
-    'eth_getCompilers',
-    'eth_newPendingTransactionFilter',
-    'eth_getWork',
-
 ]
 import poloniex
 
@@ -65,63 +51,73 @@ def node_request (command, args = []):
 
 # маршрут по умолчанию стартовая страница
 @app.route("/")
-# функция index возвращает шаблон index.html
+# функция index возвращает шаблон index.html c переменными курса валют от BTC и USD
+# ЧТО НЕ СДЕЛАНО!
+# НЕОБХОДИМО РЕАЛИЗОВАТЬ ПОЛУЧЕНИЕ ИНФОРМАЦИИ О КОЛИЧЕСТВЕ WORKERS НА ПУЛЕ ВЫВЕСТИ ТАБЛИЦУ ТОП 10 МАЙНЕРОВ ПО HASHRATE
 def index():
+	# price курс BTC_ETH
 	ticker = polo.api('returnTicker')
 	allprice = (ticker['BTC_ETH'])
 	price1 = allprice['last']
 	price = price1[0:6]
-
+	# priceUSD курс USDT_ETH
 	ticker2 = polo.api('returnTicker')
 	allprice2 = (ticker2['USDT_ETH'])
         price2 = allprice2['last']
 	priceUSD = price2[0:5]
-
-
+	# запрос хэшрейтинга пула и количества блоков сети
 	c = EthJsonRpc('localhost', 8545)
 	Hashrate = c.eth_hashrate()
 	Blocks = c.eth_blockNumber()
 	return render_template('index.html', price=price, priceUSD=priceUSD, Hashrate=Hashrate, Blocks=Blocks, cround=cround, server=SERVER_POOL)
 
-# маршрут по умолчанию стартовая блоки
+# маршрут блоки возвращает blocks.html
 @app.route("/blocks")
-# функция blocks возвращает шаблон blocks.html
+# функция blocks возвращает шаблон blocks.html в настоящее время не реализована
+
 def blocks ():
-        c = EthJsonRpc('localhost', 8545)
-	r = c.eth_accounts()
-	return render_template('blocks.html', r=r, cround=cround, server=SERVER_POOL)
+	# ЧТО НЕ СДЕЛАНО!
+	# Необходимо сформировать вывод таблицы найденных пулом блоков
+	return render_template('blocks.html',cround=cround, server=SERVER_POOL)
 
-# маршрут по умолчанию стартовая блоки
+# маршрут статистика возвращает stats.html
 @app.route("/stats")
-# функция blocks возвращает шаблон stats.html
+# функция stats возвращает шаблон stats.html в настоящее время не реализована
 def stats ():
-        c = EthJsonRpc('localhost', 8545)
-	r = c.eth_accounts()
-	return render_template('stats.html', r=r, cround=cround, server=SERVER_POOL)
+	# ЧТО НЕ СДЕЛАНО!
+	# Необходимо сформировать вывод графика за 24 часа по Хэшретйнгу и Найденным блокам
+	return render_template('stats.html', cround=cround, server=SERVER_POOL)
 
-# маршрут по умолчанию стартовая блоки
+# маршрут справка возвращает faq.html
 @app.route("/faq")
-# функция blocks возвращает шаблон faq.html
+# функция faqs возвращает шаблон faq.html реализован
 def faq ():
-        c = EthJsonRpc('localhost', 8545)
-	r = c.eth_accounts()
-	return render_template('faq.html', r=r, cround=cround, server=SERVER_POOL)
+	# ЧТО НЕ СДЕЛАНО!
+	# Необходимо прописать свои настроки в справке для подлючения
+	return render_template('faq.html', cround=cround, server=SERVER_POOL)
 
-# маршрут по умолчанию стартовая блоки
+# маршрут кредиты возвращает шаблон credits.html
 @app.route("/credits")
-# функция blocks возвращает шаблон blocks.html
+# функция credits возвращает шаблон credits.html
 def credits ():
+	# Запрос аккаунттов пула переменная 'r' и 'r1' приходит в виде списка
+	# Переменные 'r2' и  'item' первая и вторая строка списка
         c = EthJsonRpc('localhost', 8545)
 	r = c.eth_accounts()
 	r1 = c.eth_accounts()
 	r2 = r1.pop(0)
 	item = r.pop(1)
+	# ЧТО НЕ СДЕЛАНО!
+	# Необходимо сформировать таблицу аккаунт, хэшрейт, шары
 	return render_template('credits.html', r=r, r1=r1, r2=r2, item=item, cround=cround, server=SERVER_POOL)
 
-
-
+# маршрут майнер возвращает шаблон miner.html с переменными аккаунт, выплаты, шары
 @app.route("/miner", methods=['POST'])
+# функция miner возвращает шаблон miner.html
 def miner ():
+	# ЧТО НЕ СДЕЛАНО!
+	# Необходимо вывести количество workers, общий хэшрейт майнера
+	# Необходимо сформировать таблицу название workers,, хэшрейт workers, усреднённый хэшрейт workers, шары workers
 	address = request.form['address'].replace ('0x', '')
 	payouts = []
 	paylock.acquire ()
@@ -140,14 +136,7 @@ def miner ():
 	else:
 		rshare = 0
 	print rshare, cround
-
-
-
-
-      
 	return render_template('miner.html',  address=address, payouts=payouts, shares=rshare)
-
-
 
 @app.route("/submit", methods=['POST'])
 def submitShare ():
@@ -162,11 +151,11 @@ def foundBlock ():
 	bl = True
 	bllock.release ()
 
-
-def sendTransaction (address, value):
-
+# Функция выплаты  используються две переменные address - адрес куда отправить, value - сколько отправить
+def sendTransaction (address, value)
+	# tx составная переменная для запроса на выплату, COINBASE константа указывается ранее c этого адреса уходит перевод
 	tx = { 'from': COINBASE, 'to': address, 'value': value }
-  
+	# функция node_request запрос в geth на выплату в соотвествии с указанными в строке параметрами
 	node_request ('eth_sendTransaction', [tx])
 
 
@@ -245,14 +234,6 @@ if __name__ == "__main__":
 			conn = sqlite3.connect(DBPAYOUT_FILE)
 			db = conn.cursor()
 			db.execute('''CREATE TABLE payout (miner text, shares int, roundshares int, amount real, time text)''')
-			conn.commit()
-			conn.close()
-		except:
-			pass
-                try:
-			conn = sqlite3.connect(DBMINER_FILE)
-			db = conn.cursor()
-			db.execute('''CREATE TABLE payout (miner text, hashrate int, workers int)''')
 			conn.commit()
 			conn.close()
 		except:
