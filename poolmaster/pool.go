22aa19e3e62dc4ce7f5857192cccccd30bb7ce91
Bravo <djdram@ya.ru>
2016-09-04 23:39:20 +0300

    Update pool.go

--- poolmaster/pool.go
+++ poolmaster/pool.go
@@ -14,6 +14,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	// online package actual downloads, offline build fast
 	"github.com/ethereum/ethash"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/gorilla/mux"
